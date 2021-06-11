1add5f8093c899e62385621f0bcb0be486ac6f8f
Rustam Aktuganov <aktuganov@yandex.ru>
2021-06-11 18:34:09 +0200

    functional programming version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,44 +1,37 @@
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main {
+
     public static void main(String[] args) {
+         final int MIN_FREQUENCY = 1; //вывод слов с заданным количеством повторений
+         final String TEXT = "Eigentlich sind die Hotellobbys " +
+                "zu dieser Zeit voll. Touristen stehen am Frühstücksbuffet " +
+                "an, vor Museen und Sehenswürdigkeiten bilden sich lange " +
+                "Warteschlangen. Das ist heuer – zum zweiten " +
+                "Mal in Folge – anders. \"Die internationalen Touristen " +
+                "werden wohl noch einige Zeit auf sich warten lassen\", " +
+                "sagt Michaela Reitterer, Betreiberin des Boutiquehotels " +
+                "Stadthalle und Präsidentin der Österreichischen " +
+                "Hoteliervereinigung (ÖHV). Am Wochenende würden immer " +
+                "mehr Gäste kommen. Unter der Woche spüre man aber nach " +
+                "wie vor den Ausfall bei Geschäftsreisenden und Kongresstouristen.\n";
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("For generating a new language dictionary " +
-                "you can put phrases or texts into the system, " +
-                "when you are done just tap 'end'");
-
-        List <String> text = new ArrayList<>();
-
-        while(true){
-            System.out.println("\nPlease put next phrase/text or tap 'end'");
-            String input = scanner.nextLine();
-            if ("end".equals(input)) {
-                break;
-            }
-            text.add(input);
-        }
-
-        List <String> words = text.stream()
-                .flatMap(x -> Arrays.stream(x.split("\\b")))
-                .map(String::toUpperCase)
-                .sorted(Comparator.naturalOrder())
+        //функция высшего порядка
+        //детерминированная
+        //без побочных эффектов
+        //используется монада
+         List <String> dictionary = words.apply(TEXT).stream()
+                .filter(e -> Collections.frequency(words.apply(TEXT), e) >= MIN_FREQUENCY)
                 .collect(Collectors.toList());
 
-        List <String> dublicates = words.stream().filter(e -> Collections.frequency(words, e) > 1)
-                .collect(Collectors.toList());
-
-        dublicates.stream()
+        dictionary.stream()
                 .forEach(System.out::println);
 
-        words.stream()
-                .distinct()
-                .forEach(System.out::println);
-
-        scanner.close();
     }
+    public static Function<String, List<String>> words = x -> Arrays.stream(x.split("\\b"))
+            .map(String::toUpperCase)
+            .sorted(Comparator.naturalOrder())
+            .collect(Collectors.toList());
 }
-/*
-Eigentlich sind die Hotellobbys zu dieser Zeit voll. Touristen stehen am Frühstücksbuffet an, vor Museen und Sehenswürdigkeiten bilden sich lange Warteschlangen. Das ist heuer – zum zweiten Mal in Folge – anders. "Die internationalen Touristen werden wohl noch einige Zeit auf sich warten lassen", sagt Michaela Reitterer, Betreiberin des Boutiquehotels Stadthalle und Präsidentin der Österreichischen Hoteliervereinigung (ÖHV). Am Wochenende würden immer mehr Gäste kommen. Unter der Woche spüre man aber nach wie vor den Ausfall bei Geschäftsreisenden und Kongresstouristen.
-*/
